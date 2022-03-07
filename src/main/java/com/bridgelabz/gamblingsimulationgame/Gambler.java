ab87fa1abbd3f8f1cad9c762dee6ac0f34516fa3
mangesh9903 <mangeshb9903@gmail.com>
2022-03-07 23:22:42 +0530

    UseCase ( Checking Player Win or Lose using limit condition ) 3 Commited

--- src/main/java/com/bridgelabz/gamblingsimulationgame/Gambler.java
+++ src/main/java/com/bridgelabz/gamblingsimulationgame/Gambler.java
@@ -1,17 +1,29 @@
 package com.bridgelabz.gamblingsimulationgame;
 
+import java.util.Scanner;
+
 public class Gambler {
     public static final int STAKE = 100;
     public static final int BET = 1;
 
     public static void main(String[] args) {
         Gambler gambler = new Gambler();
+        Scanner scanner = new Scanner(System.in);
         System.out.println("******* Welcome to Gambler Game *******");
-        gambler.checkWinOrLose();
+        System.out.println("Set Limit till you want to win ");
+        int winLimit = scanner.nextInt();
+        System.out.println("Set Limit till you can afford Lose");
+        int loseLimit = scanner.nextInt();
+        gambler.gamePlay(winLimit, loseLimit);
         System.out.println("******* Game Finished *******");
     }
 
-    public void checkWinOrLose() {
+    /**
+     * Usecase 2 ( Checking If player Win Or Lose )
+     *
+     * @return
+     */
+    public boolean checkWinOrLose() {
         boolean randomlyCheck = (Math.random() < 0.5);
 
         if (randomlyCheck) {
@@ -19,5 +31,27 @@ public class Gambler {
         } else {
             System.out.println(" Player Lose ");
         }
+        return randomlyCheck;
+    }
+
+    /**
+     * UseCase 3 ( Checking player Win or Lose using Limits )
+     *
+     * @param winLimit  player will stop playing game when reaches to winLimit
+     * @param loseLimit player will stop playing game when reches to loseLimit
+     * @return stake    It will return the result whether player Win or Lose
+     */
+    public int gamePlay(int winLimit, int loseLimit) {
+        int stake = 100;
+        while (stake < winLimit && stake > loseLimit) {
+            if (checkWinOrLose()) {
+                stake = stake + 1;
+                System.out.println(stake + " ");
+            } else {
+                stake = stake - 1;
+                System.out.println(stake + " ");
+            }
+        }
+        return stake;
     }
 }
---
+++ src/test/java/com/bridgelabz/testgamblingsimulationgame/GamblerTest.java
@@ -0,0 +1,38 @@
+package com.bridgelabz.testgamblingsimulationgame;
+
+import com.bridgelabz.gamblingsimulationgame.Gambler;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GamblerTest {
+
+    Gambler gambler = new Gambler();
+
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println(" Testing started...");
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        System.out.println(" Testing Finished...");
+    }
+
+    @Test
+    public void testGamePlay_Win() {
+
+        assertEquals(150, gambler.gamePlay(150, 50));
+
+    }
+
+    @Test
+    public void testGamePlay_Lose() {
+
+        assertEquals(50, gambler.gamePlay(150, 50));
+
+    }
+
+}
