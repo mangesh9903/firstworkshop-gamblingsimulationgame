a637d3a5f4348dcf6eed9610f52bc733ef74c7e7
mangesh9903 <mangeshb9903@gmail.com>
2022-03-07 16:43:02 +0530

    UseCase1 ( StakeAndBet Initialized ) commited

--- src/main/java/com/bridgelabz/gamblingsimulationgame/Gambler.java
+++ src/main/java/com/bridgelabz/gamblingsimulationgame/Gambler.java
@@ -1,5 +1,10 @@
 package com.bridgelabz.gamblingsimulationgame;
 
 public class Gambler {
+    public static final int STAKE = 100;
+    public static final int BET = 1;
 
+    public static void main(String[] args) {
+        System.out.println("******* Welcome to Gambler Game *******");
+    }
 }
